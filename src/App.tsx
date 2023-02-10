c0b26c33d214fea3dcf9b91d830ba8ee2593803f
JosiahFu <josiahfu@gmail.com>
2023-02-10 14:42:25 -0800

    Lorem ipsum + outlines + dropdowns

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { LoremIpsum } from 'lorem-ipsum';
 import './App.css';
 import Venn, { stringSet } from './Venn';
 
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 4,
+      min: 2
+    },
+    wordsPerSentence: {
+      max: 10,
+      min: 8
+    }
+});
+
 function App() {
     const processInput = (input: stringSet): stringSet => {
-        return ['Option 1', 'Option 2', 'Option 3', 'Option 4']
+        return [
+            lorem.generateParagraphs(1), 
+            lorem.generateParagraphs(1), 
+            lorem.generateParagraphs(1), 
+            lorem.generateParagraphs(1), 
+        ];
     }
 
     return (
         <main>
-            <Venn processInput={processInput}/>
+            <Venn processInput={processInput} options={[
+                ['a', 'b', 'c', 'd'],
+                ['a', 'b', 'c', 'd'],
+                ['a', 'b', 'c', 'd'],
+                ['a', 'b', 'c', 'd'],
+            ]}/>
         </main>
     );
 }
--- src/Venn.tsx
+++ src/Venn.tsx
@@ -3,25 +3,56 @@ import './Venn.css';
 
 type stringSet = readonly [string, string, string, string];
 
-function Circle({input, setInput, output}: {
-    input: string;
-    setInput: (input: string) => void;
-    output: string;
+function Dropdown({options, value, setValue}: {
+    options: string[],
+    value: string,
+    setValue: (value: string) => void
 }) {
+    const [expanded, setExpanded] = useState(false);
+
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(event.target.value);
+        setValue(event.target.value)
     }
 
     return (
+        <div className="dropdown">
+            <input
+                value={value}
+                onChange={handleInput}
+                onFocus={() => setExpanded(true)}
+                onBlur={() => setTimeout(() => setExpanded(false), 100)} // Required for some reason?
+            />
+            {expanded && <div className="dropdown-option-list">
+                {options
+                    .filter(e => e.includes(value))
+                    .map((e, i) => <div
+                        key={i}
+                        className="dropdown-option"
+                        onClick={() => setValue(e)}>{e}
+                    </div>)}
+            </div>}
+        </div>
+    )
+}
+
+function Circle({input, setInput, output, options}: {
+    input: string,
+    setInput: (input: string) => void,
+    output: string,
+    options: string[]
+}) {
+
+    return (
         <div className="circle">
-            <input value={input} onInput={handleInput} />
-            <span className="output">{output}</span>
+            <Dropdown options={options} value={input} setValue={setInput} />
+            <div className="output">{output}</div>
         </div>
     );
 }
 
-function Venn({processInput}: {
-    processInput: (input: stringSet) => stringSet;
+function Venn({options, processInput}: {
+    options: [string[], string[], string[], string[]],
+    processInput: (input: stringSet) => stringSet
 }) {
     const [activated, setActivated] = useState(false);
     const [input1, setInput1] = useState('');
@@ -37,12 +68,12 @@ function Venn({processInput}: {
     }
 
     return (
-        <div className="venn">
-            <div className={activated ? 'circles activated' : 'circles'}>
-                <Circle input={input1} setInput={setInput1} output={output[0]} />
-                <Circle input={input2} setInput={setInput2} output={output[1]} />
-                <Circle input={input3} setInput={setInput3} output={output[2]} />
-                <Circle input={input4} setInput={setInput4} output={output[3]} />
+        <div className={activated ? 'venn activated' : 'venn'}>
+            <div className="circles">
+                <Circle input={input1} setInput={setInput1} output={output[0]} options={options[0]} />
+                <Circle input={input2} setInput={setInput2} output={output[1]} options={options[1]} />
+                <Circle input={input3} setInput={setInput3} output={output[2]} options={options[2]} />
+                <Circle input={input4} setInput={setInput4} output={output[3]} options={options[3]} />
             </div>
             <div className="button" onClick={handleClick}>Press</div>
         </div>
