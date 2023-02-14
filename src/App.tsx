d953c66e2795e01a3fa557bf49befbbc44ef93e9
Josiah Fu <69983796+JosiahFu@users.noreply.github.com>
2023-02-14 10:41:05 -0800

    Remove lorem ipsum

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
-import { LoremIpsum } from 'lorem-ipsum';
 import './App.css';
 import Venn, { stringSet } from './Venn';
-
-const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-        max: 4,
-        min: 2
-    },
-    wordsPerSentence: {
-        max: 10,
-        min: 8
-    }
-});
 
 function App() {
     const processInput = (input: stringSet): stringSet => {
